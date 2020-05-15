6a24fa579eda5c0627929da58054b4dd7700c370
bvadell <bvadell@intricom.es>
2020-05-15 11:37:55 +0200

    ApiServer - Server UI
    Add packages ASPNET CORE MVC
    Upgrade from 4.6.1 to 4.7.2
    Create ServerController

---
+++ ServerLibrary/Controllers/ServerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Envir;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServerController
+    {
+        [HttpGet("status")]
+        public ServerStatus GetStatus()
+        {
+            return new ServerStatus
+            {
+                Online = SEnvir.Started,
+                Players = SEnvir.Players.Count
+            };
+        }
+    }
+}
---
+++ ServerLibrary/Models/ServerStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Models
+{
+    public class ServerStatus
+    {
+        public bool Online { get; set; }
+        public int Players { get; set; }
+    }
+}
